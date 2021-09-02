c934bb42972dda49d78252b5935f92b554b9a177
nhatvu148 <nhatvu148@gmail.com>
2021-09-02 16:31:59 +0900

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import IMAGE from "./react.jpg";
 import { Counter } from "./Counter";
 
 export const App = () => {
-    return (
-        <>
-            <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
-            <img src={IMAGE} alt="React Logo" width="300" height="200" />
-            <Counter />
-        </>
-    )
-}
+  return (
+    <>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <Counter />
+    </>
+  );
+};
